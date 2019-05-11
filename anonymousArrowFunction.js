e8121489ee91b8bf8a4b0ec23d2c398b74170f83
rudwear <rudwear@gmail.com>
2019-05-11 12:24:23 +0900

    anonymous Arrow function

---
+++ anonymousArrowFunction.js
@@ -0,0 +1,16 @@
+// Arrow functinos to write concise anonymous function
+
+const myFunc = function(){
+	const myVar = "value";
+	return myVar;
+}
+
+const myFunc1 = () => {
+	const myVar = "value";
+	return myVar;
+}
+
+const myFunc2 = ()=>"value"
+myFunc3 = (name) => {return "My name is " + name}
+// myFunc3("Hello")
+// result : My name is Hello
